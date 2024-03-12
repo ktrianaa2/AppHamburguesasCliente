17ea017b06521da81837d3c15da374bc0c930515
Luis <lreyesp@uteq.edu.ec>
2024-03-11 19:27:00 -0500

    Funciona el idCuenta

--- app/src/main/java/com/example/apphamburguesascliente/BotonUbicacionConfiguradaFragment.java
+++ app/src/main/java/com/example/apphamburguesascliente/BotonUbicacionConfiguradaFragment.java
@@ -1,6 +1,9 @@
 package com.example.apphamburguesascliente;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +16,8 @@ import android.widget.Button;
 
 public class BotonUbicacionConfiguradaFragment extends Fragment {
 
+    private int idCliente = -1; // Valor predeterminado o valor que consideres adecuado
+
     public BotonUbicacionConfiguradaFragment() {
         // Required empty public constructor
     }
@@ -22,33 +27,32 @@ public class BotonUbicacionConfiguradaFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_boton_ubicacion_configurada, container, false);
 
-        // Obtén la ID del usuario y la ubicación desde los argumentos
-        int idUsuario = getArguments().getInt("idUsuario");
+        // Obtener el idCliente desde SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        idCliente = sharedPreferences.getInt("id_cuenta", -1); // Usamos -1 como valor predeterminado si no se encuentra
+
+        // Mostrar el idCliente en Logcat
+        Log.d("BotonUbicacionConfig", "El idCliente es: " + idCliente);
+
         double latitud = getArguments().getDouble("latitud");
         double longitud = getArguments().getDouble("longitud");
 
-        // Accede al botón de editar y añade un OnClickListener
+        // Configura el botón de editar y añade un OnClickListener
         Button editButton = view.findViewById(R.id.editbtn);
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Abre la actividad correspondiente para la edición
-                Intent intent = new Intent(getActivity(), EditarUbicacionActivity.class);
-                intent.putExtra("idUsuario", idUsuario);
-                intent.putExtra("latitud", latitud);
-                intent.putExtra("longitud", longitud);
-                startActivity(intent);
-            }
+        editButton.setOnClickListener(v -> {
+            // Abre la actividad correspondiente para la edición
+            Intent intent = new Intent(getActivity(), EditarUbicacionActivity.class);
+            intent.putExtra("idUsuario", idCliente);
+            intent.putExtra("latitud", latitud);
+            intent.putExtra("longitud", longitud);
+            startActivity(intent);
         });
 
-        // En BotonUbicacionConfiguradaFragment y BotonUbicacionNoConfiguradaFragment
+        // Configura el botón de eliminar y añade un OnClickListener
         Button deleteButton = view.findViewById(R.id.deletebtn);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Aquí puedes agregar la lógica para eliminar la ubicación
-                Log.d("EliminarUbicacion", "Eliminar ubicación para el usuario con ID: " + idUsuario);
-            }
+        deleteButton.setOnClickListener(v -> {
+            // Aquí puedes agregar la lógica para eliminar la ubicación
+            Log.d("EliminarUbicacion", "Eliminar ubicación para el usuario con ID: " + idCliente + latitud + longitud);
         });
 
         return view;
